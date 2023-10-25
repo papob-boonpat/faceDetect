027617d152fa3f26468cd0bdf8ba7976a8e7f43c
boonpat.papob <boonpat.papob@gmail.com>
2023-10-25 15:43:07 +0700

    Initial Commit

---
+++ faceReg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  faceReg
+//
+//  Created by papob boonpat on 25/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
